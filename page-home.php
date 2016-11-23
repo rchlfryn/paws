9dce2e9b5f9d42238755cc94497b4d7928eddc10
Rachel Fryan <rachelafryan@gmail.com>
2016-11-23 07:00:00 -0600

    updating home

--- page-home.php
+++ page-home.php
@@ -13,27 +13,51 @@
  */
 
 get_header(); ?>
+<?php 
+$hero_background = get_field('hero_background');
+$about_photo = get_field('about_photo');
+$id_camp_photo = get_field('id_camp_photo');
+$testimonial = get_field('testimonial');
+?>
+<!-- Hero section -->
+<div class="hero">
+
+	<div id="hero-background" style="background: url(<?php echo $hero_background['url']; ?>); background-size: cover;">
+		<button class="hero-cta">Call to actisson</button>
+	</div>
+</div>
 	<div class="content-container">
-	<div id="primary" class="content-area">
+
+
+
 		<main id="main" role="main">
+			<div class="main-content">
+				<div class="boxes">
+					<div class="testimonial" >
+						<?php the_field('testimonial'); ?> 
+					</div>
+					<div class="calendar"><?php the_field('calendar_link'); ?></div>
+					<div class="box" style="background: url(<?php echo $about_photo['url']; ?>); background-size: cover;">
+						<button onclick="location.href='/training'" class="overlay-button">Training</button>
+					</div>
+					<div class="box" style="background: url(<?php echo $id_camp_photo['url']; ?>); background-size: cover;">
+						<button onclick="location.href='/training'" class="overlay-button">Training</button>
+					</div>
 
-		<!-- Hero section -->
-		<div class="hero">
-			<img src="http://placehold.it/1100x500">
-			<button class="hero-cta">Call to action</button>
-		</div>
 
-		<div class="main-content">
-			<div class="boxes">
-				<div class="box"></div>
-				<div class="box"></div>
-				<div class="box"></div>
-				<div class="box"></div>
+				</div>
+					<div class="main-text-area">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								<?php
+									// the content (pretty self explanatory huh)
+									the_content();
+								?>
+
+						<?php endwhile; endif; ?>
+					</div>
 			</div>
-		</div>
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
 </div>
 <?php
 get_footer();
